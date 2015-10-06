3a8b001e4ea4c46e54dd0b974c871ec6ecc2df5a
MichaelFeng <michaelfeng.smart@gmail.com>
2015-10-06 22:55:42 +0800

    finish static pages

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "搞基网 | 一家专业搞基的网站"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "搞基求助 | 一家专业搞基的网站"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "关于搞基 | 一家专业搞基的网站"
+  end
 end
