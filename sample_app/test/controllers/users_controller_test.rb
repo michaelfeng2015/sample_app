3d384840306d1e8e8f59ed2cc4e240bd936d852e
MichaelFeng <michaelfeng.smart@gmail.com>
2015-10-07 18:52:33 +0800

    finish user model

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", "加入搞基 | 一家专业搞基的网站"
   end
 
 end
