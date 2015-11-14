e7d3d24f5f0b98bc1ab1fa8c644833f75356bd7e
MichaelFeng <michaelfeng.smart@gmail.com>
2015-11-14 20:10:27 +0800

    add deploy

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # 登陆用户，然后重定向到用户界面
       log_in(user)
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       #创建一个错误信息,返回登陆界面
       flash.now[:danger] = '邮箱或密码不正确哟！'
