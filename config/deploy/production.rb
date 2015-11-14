3c00fd3aadb2e22fe5f38d1ccc5352d8f488896a
MichaelFeng <michaelfeng.smart@gmail.com>
2015-11-15 04:53:33 +0800

    fix configure

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@smartmichael.com}
-role :web, %w{deploy@smartmichael.com}
-role :db,  %w{deploy@smartmichael.com}
+# role :app, %w{deploy@smartmichael.com}
+# role :web, %w{deploy@smartmichael.com}
+# role :db,  %w{deploy@smartmichael.com}
 
 set :stage, :production
 
